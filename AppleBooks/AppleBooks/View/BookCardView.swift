9f4f0a0db08481a3a9022ce7aec7cbae91a91004
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-26 19:45:14 +0100

    Book Card View Scroll in progress

--- AppleBooks/AppleBooks/View/BookCardView.swift
+++ AppleBooks/AppleBooks/View/BookCardView.swift
@@ -10,17 +10,32 @@ import SwiftUI
 
 struct BookCardView: View {
     let book: Book
+    let size: CGSize
+    // Scroll Animation Properties
+    @State private var scrollProperties: ScrollGeometry = .init(contentOffset: .zero, contentSize: .zero, contentInsets: .init(), containerSize: .zero)
+    @State private var scrollPosition: ScrollPosition = .init()
     
     
-    init(book: Book) {
+    init(book: Book, size: CGSize) {
         self.book = book
+        self.size = size
     }
     
     var body: some View {
         ScrollView(.vertical) {
             VStack(alignment: .leading, spacing: 15) {
                 TopCardView()
+                    .containerRelativeFrame(.vertical) { value, _ in
+                        value * 0.9
+                    }
+                
+                OtherTextContents()
             }
+        }
+        .background {
+            UnevenRoundedRectangle(topLeadingRadius: 15, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 15)
+                .fill(.background)
+                .ignoresSafeArea(.all, edges: .bottom)
         }
     }
     
@@ -61,41 +76,87 @@ struct BookCardView: View {
                     
                     Image(systemName: "info.circle")
                         .font(.caption)
-                    
-                    Text("45 pages")
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
-                    
-                    HStack(spacing: 10) {
-                        Button {
-                            
-                        } label: {
-                            Label("Sample", systemImage: "book.pages")
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 5)
-                        }
-                        .tint(.white.opacity(0.2))
-
-                        Button {
-                            
-                        } label: {
-                            Text("Get")
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 5)
-                        }
-                        .foregroundStyle(.black)
-                        .tint(.white)
-                    }
-                    .buttonStyle(.borderedProminent)
                 }
+                
+                Text("45 pages")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                
+                HStack(spacing: 10) {
+                    Button {
+                        
+                    } label: {
+                        Label("Sample", systemImage: "book.pages")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                    }
+                    .tint(.white.opacity(0.2))
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Get")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 5)
+                    }
+                    .foregroundStyle(.black)
+                    .tint(.white)
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.top, 5)
             }
-
+            .padding(15)
+            .background(.white.opacity(0.2), in: .rect(cornerRadius:15))
         }
         .foregroundStyle(.white)
+        .padding(15)
+        .frame(maxWidth: .infinity)
         .background {
             Rectangle()
                 .fill(book.color.gradient)
         }
+        .clipShape(UnevenRoundedRectangle(topLeadingRadius: 15, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 15))
+    }
+    
+    func OtherTextContents() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("From the Publisher")
+                .serifText(.title, weight: .semibold)
+            
+            Text(dummyText)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+                .lineLimit(5)
+            
+            Text("Requirements")
+                .serifText(.title3, weight: .semibold)
+                .padding(.top, 15)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Apple Books")
+                
+                Text("Requires iOS 12 or macOS 10.14 or later")
+                    .foregroundStyle(.secondary)
+                
+                Text("iBooks")
+                    .padding(.top, 5)
+                
+                Text("Requires iBooks 3 or iOS 4.3 or later")
+                    .foregroundStyle(.secondary)
+                
+                Text("Versions")
+                    .font(.title3)
+                    .fontDesign(.serif)
+                    .fontWeight(.semibold)
+                    .padding(.top, 25)
+                
+                Text("Update Mar 16 2022")
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.top, 5)
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
     }
     
     func FixedHeaderView() -> some View {
@@ -128,7 +189,11 @@ struct BookCardView: View {
 
 
 #Preview {
-    BookCardView(book: books[0])
+    GeometryReader { geometry in
+        BookCardView(book: books[0], size: geometry.size)
+    }
+    .padding(.horizontal, 15)
+    .background(.gray.opacity(0.15))
 }
 
 
