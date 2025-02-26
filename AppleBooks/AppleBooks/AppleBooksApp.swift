88014733e8246877b292ced1cb7b0450c8bd67b1
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-26 12:02:12 +0100

    Create project

---
+++ AppleBooks/AppleBooks/AppleBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleBooksApp.swift
+//  AppleBooks
+//
+//  Created by Jesus Antonio Gil on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
